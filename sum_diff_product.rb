ff8fd947aaa7040c93e2c59bb6eced94f535d4a1
Tasha Lawwson <tlawson@chicagotechacademy.org>
2013-11-07 13:22:03 -0600

    classwork

---
+++ sum_diff_product.rb
@@ -0,0 +1,14 @@
+puts "Submit a number"
+first_num = gets.chomp
+
+puts "Submit a second number"
+second_num = gets.chomp
+
+num_sum = first_num.to_i + second_num.to_i
+num_diff = first_num.to_i - second_num.to_i
+num_pro = first_num.to_i * second_num.to_i 
+
+puts ""
+puts num_sum
+puts num_diff
+puts num_pro
